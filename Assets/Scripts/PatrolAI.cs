edb2ccb36f13ddd48d2d54657ed9cd70a6826e2e
sachinlele7 <sachinlele12@gmail.com>
2021-07-28 17:36:22 +0530

    More Than 1 Enemy on the platform

---
+++ Assets/Scripts/PatrolAI.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolAI : MonoBehaviour
+{
+    public float walkSpeed;
+
+    [HideInInspector]
+    public bool mustPatrol;
+    private bool MustTurn;
+    public Rigidbody2D rb;
+    public Transform GroundDetect;
+    public LayerMask groundLayer;
+    public LayerMask ground1Layer;
+    public Collider2D bodyCollider;
+
+    private void Start()
+    {
+        mustPatrol = true;
+    }
+
+    private void Update()
+    {
+        if (mustPatrol)
+        {
+            Patrol();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(mustPatrol)
+        {
+            MustTurn = !Physics2D.OverlapCircle(GroundDetect.position, 0.1f, groundLayer);
+        }
+    }
+    private void Patrol()
+    {
+        if(MustTurn || bodyCollider.IsTouchingLayers(groundLayer))
+        {
+            Flip();
+        }
+        
+        rb.velocity = new Vector2(walkSpeed * Time.fixedDeltaTime, rb.velocity.y);
+    }
+
+    void Flip()
+    {
+        mustPatrol = false;
+        transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
+        walkSpeed *= -1;
+        mustPatrol = true;
+    }
+
+
+}
